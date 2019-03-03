8719bd51aa64df2ccdc796d99c644d6153fc0427
Bazzer588 <40038437+Bazzer588@users.noreply.github.com>
2019-03-03 11:11:23 +0000

    hooky

---
+++ hooky/precache-manifest.ffd9c92bbc2c745c5b553b67b0cfbdab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hooky/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "440da793b702d3d7244b",
+    "url": "/hooky/static/js/runtime~main.440da793.js"
+  },
+  {
+    "revision": "d46aa9329464fe4469d3",
+    "url": "/hooky/static/js/main.d46aa932.chunk.js"
+  },
+  {
+    "revision": "549cdd6bd6ad42a7c581",
+    "url": "/hooky/static/js/2.549cdd6b.chunk.js"
+  },
+  {
+    "revision": "d46aa9329464fe4469d3",
+    "url": "/hooky/static/css/main.548b7c3f.chunk.css"
+  },
+  {
+    "revision": "d8c5e4421d4cf464b6e1174ee86cbadb",
+    "url": "/hooky/index.html"
+  }
+];
